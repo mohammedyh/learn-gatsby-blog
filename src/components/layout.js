e92fc254369c91af5c2d48ea79541781f1de291e
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-26 23:43:39 +0000

    Style layout using css modules

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,31 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import {
+	container,
+	heading,
+	navLinkItem,
+	navLinks,
+	navLinkText,
+} from './layout.module.css';
 
 const Layout = ({ children, pageTitle }) => (
-	<div>
+	<div className={container}>
 		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
+			<ul className={navLinks}>
+				<li className={navLinkItem}>
+					<Link to="/" className={navLinkText}>
+						Home
+					</Link>
 				</li>
-				<li>
-					<Link to="/about">About</Link>
+				<li className={navLinkItem}>
+					<Link to="/about" className={navLinkText}>
+						About
+					</Link>
 				</li>
 			</ul>
 		</nav>
 		<main>
-			<h1>{pageTitle}</h1>
+			<h1 className={heading}>{pageTitle}</h1>
 			{children}
 		</main>
 	</div>
