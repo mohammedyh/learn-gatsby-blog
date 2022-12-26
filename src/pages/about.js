4d52dbcca85692a819b40047f1b4bb8cf5d35286
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-26 22:54:46 +0000

    Add link to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 const AboutPage = () => (
 	<main>
 		<h1>About me</h1>
+		<Link to="/">Back to home</Link>
 		<p>
 			Hi there! I'm the proud creator of this site, which I built with Gatsby.
 		</p>
