66d3ffd0016c4d74497761cab3f2227cf0bc98a1
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-26 22:51:54 +0000

    Modify index page and create about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => (
+	<main>
+		<h1>About me</h1>
+		<p>
+			Hi there! I'm the proud creator of this site, which I built with Gatsby.
+		</p>
+	</main>
+);
+
+export default AboutPage;
+
+export const Head = () => <title>About me</title>;
