becb39a68cb7b737378cf2b862f827c348aa0dc5
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-27 18:09:12 +0000

    Add site title to layout using getStaticQuery

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 import {
 	container,
@@ -6,29 +6,47 @@ import {
 	navLinkItem,
 	navLinks,
 	navLinkText,
+	siteTitle,
 } from './layout.module.css';
 
-const Layout = ({ children, pageTitle }) => (
-	<div className={container}>
-		<nav>
-			<ul className={navLinks}>
-				<li className={navLinkItem}>
-					<Link to="/" className={navLinkText}>
-						Home
-					</Link>
-				</li>
-				<li className={navLinkItem}>
-					<Link to="/about" className={navLinkText}>
-						About
-					</Link>
-				</li>
-			</ul>
-		</nav>
-		<main>
-			<h1 className={heading}>{pageTitle}</h1>
-			{children}
-		</main>
-	</div>
-);
+const Layout = ({ children, pageTitle }) => {
+	const data = useStaticQuery(graphql`
+		query MyQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`);
+	return (
+		<div className={container}>
+			<header className={siteTitle}>{data.site.siteMetadata.title}</header>
+			<nav>
+				<ul className={navLinks}>
+					<li className={navLinkItem}>
+						<Link to="/" className={navLinkText}>
+							Home
+						</Link>
+					</li>
+					<li className={navLinkItem}>
+						<Link to="/about" className={navLinkText}>
+							About
+						</Link>
+					</li>
+					<li className={navLinkItem}>
+						<Link to="/blog" className={navLinkText}>
+							Blog
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<main>
+				<h1 className={heading}>{pageTitle}</h1>
+				{children}
+			</main>
+		</div>
+	);
+};
 
 export default Layout;
