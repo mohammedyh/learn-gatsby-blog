4f34e02fa163a64fcd68b6408c489da36ad7e0e0
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-27 18:11:50 +0000

    Modify config file and dev script

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,19 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Blog`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
-}
+	siteMetadata: {
+		title: 'Learn Gatsby by Building a Blog',
+		siteUrl: 'https://inquisitive-bunny-9f8702.netlify.app/',
+	},
+	plugins: [
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: `blog`,
+				path: `${__dirname}/blog`,
+			},
+		},
+	],
+};
